99584d36f89b73490112b704cb9998d00a1ecca7
SandraMeshack <56250836+SandraMeshack@users.noreply.github.com>
2019-12-11 15:40:03 +0000

    Exercise1.2

---
+++ exercise1-2.java
@@ -0,0 +1,15 @@
+boolean permutation(String s, String t) {
+if (s.length() !=t.length()) return false;
+
+int[] letters =new int[128];
+for (int i=0; i< s.length(); i++){
+letter[s.charAt(i)]++;
+}
+for (int i = 0; i < t.length(); i++) {
+letters[t.charAt(i)]--;
+if(letters[t.charAt(i)] < 0){
+return false;
+}
+}
+return true;
+}
